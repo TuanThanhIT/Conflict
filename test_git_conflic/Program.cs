c60a008ab0979b4207e7c0feac6afd82ddfd7bbf
“LucAnL2” <huynhthaitoan.12c1htk@gmail.com>
2024-02-22 23:52:51 +0700

    Test GitHub

--- test_git_conflic/Program.cs
+++ test_git_conflic/Program.cs
@@ -11,6 +11,8 @@ namespace testGit
             int y = 10;
             int tong = x + y;
             Console.WriteLine(tong);
+            //Test GitHub
+            Console.WriteLine("Here");
         }
     }
 }
--- test_git_conflic/obj/Debug/net8.0/test_git_conflic.AssemblyInfo.cs
+++ test_git_conflic/obj/Debug/net8.0/test_git_conflic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test_git_conflic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3cb7041957241b392b60877192c1355ff4a61a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("test_git_conflic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test_git_conflic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
