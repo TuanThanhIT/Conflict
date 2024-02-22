c3cb7041957241b392b60877192c1355ff4a61a3
“TuanThanh“ <tuanthanh07082004@gmail.com>
2024-02-22 22:56:22 +0700

    Test conflict

---
+++ test_git_conflic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace testGit
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            int tong = x + y;
+            Console.WriteLine(tong);
+        }
+    }
+}
